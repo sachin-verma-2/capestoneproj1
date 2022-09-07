95ec348e81c11a3725d7526e3fc3880b355d4638
Khushi-0404 <khushi.1@globallogic.com>
2022-09-07 15:21:05 +0530

    Home Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Home from './Components/LandingPage/Home';
+
+import LoginPage from './Pages/LoginPage';
+import MainPage from './Pages/MainPage';
+import SignupPage from './Pages/SignupPage';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage></MainPage>}></Route>
+        <Route path="/login" element={<LoginPage></LoginPage>}></Route>
+        <Route path="/signup" element={<SignupPage></SignupPage>}></Route>
+        
+        </Routes></BrowserRouter>
+      
 
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+import { Provider } from 'react-redux';
+import store from './Store/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
+   <Provider store={store}>
     <App />
+    </Provider>
   </>
 );
 
